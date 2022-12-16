481a50ac0f29532e39e7b89fd91f568fa5fee08e
Glenn <phoobynet@users.noreply.github.com>
2022-12-16 17:04:38 +0000

    working on it.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,59 @@
+import iconLuxury from '@/assets/images/icon-luxury.svg'
+import iconSedans from '@/assets/images/icon-sedans.svg'
+import iconSuvs from '@/assets/images/icon-suvs.svg'
+
 export default function Home() {
   return (
     <div className="cards">
       <div
         id="sedans"
         className="card"
-      ></div>
+      >
+        <img
+          src={iconSedans}
+          alt=""
+        />
+        <h2>sedans</h2>
+        <p>
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
+        </p>
+        <div>
+          <button>Learn More</button>
+        </div>
+      </div>
       <div
         id="suvs"
         className="card"
-      ></div>
+      >
+        <img
+          src={iconSuvs}
+          alt=""
+        />
+        <h2>suvs</h2>
+        <p>
+          Take an SUV for its spacious interior, power, and versatility. Perfect
+          for your next family vacation and off-road adventures.
+        </p>
+        <div>
+          <button>Learn More</button>
+        </div>
+      </div>
       <div
         id="luxury"
         className="card"
-      ></div>
+      >
+        <img
+          src={iconLuxury}
+          alt=""
+        />
+        <h2>luxury</h2>
+        <p>
+          Cruise in the best car brands without the bloated prices. Enjoy the
+          enhanced comfort of a luxury rental and arrive in style.
+        </p>
+        <button>Learn More</button>
+      </div>
     </div>
   )
 }
